8f4d75c85878112fb29e1fe4b7198334bbe59c6c
mshirazkamran <blogger31625@gmail.com>
2025-04-26 19:57:39 +0500

    addeed geminia api route

--- netlify/functions/invokeGemini.mjs
+++ netlify/functions/invokeGemini.mjs
@@ -1,3 +1,6 @@
+import { GoogleGenAI } from "@google/genai";
+
+
 // Arrow function with implicit return
 export default async (req, context) => {
 
@@ -5,11 +8,33 @@ export default async (req, context) => {
     console.log(url);
     const apiKey = Netlify.env.get("GAME_KEY");
 
-    // make a new gemini request object with the acuquired api key
+    // const body = await req.json();
+    // console.log(body);
 
-    return new Response("Endpoint in development");
+    // make a new gemini request object with the acuquired api key
+    const ai = new GoogleGenAI({ apiKey: apiKey });
+
+    let responseText;
+
+    async function getAnswer(question) {
+        // if (!(question instanceof String)) {
+        //     console.log("its not a string")
+        //     return null;
+        // }
+        question = String(question);
+        const response = await ai.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: question,
+        });
+        console.log(response.text);
+        return response.text;
+    }
+
+    const dataToSend = await getAnswer("ONLY GIVE THE ANSWER IN SHORTEST WAY WITH A LITTLE EXPLANATOIN,give me a cronjob query for everyday at 8:42pm");
+
+    return new Response(dataToSend);
 }
 
 export const config = {
-    path: "api/devotol/ask"
+    path: "/api/devotol/ask"
 }
